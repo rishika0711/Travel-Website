04f7724f7a22fd6e80b5c9f68df2668f1546b4c1
Rishika Singh <rishikasingh0711@gmail.com>
2021-11-23 23:23:38 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBtn = document.querySelector('.search-bar-container');
+
+searchBtn.addEventListener('click', ()=>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+
+});
