b7ea41a4fc43c85815a994edfeb37874624b07a4
Rishika Singh <rishikasingh0711@gmail.com>
2021-11-25 22:50:13 +0530

    login page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBtn = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.search-bar-container');
 
+window.onscroll=()=>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+}
 searchBtn.addEventListener('click', ()=>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
